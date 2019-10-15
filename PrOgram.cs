65f9de2b5f38723c56572376839417143c612161
ThanabodinBoonnark <41316691+ThanabodinBoonnark@users.noreply.github.com>
2019-10-15 15:06:27 +0700

    Add files via upload

---
+++ PrOgram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace appppp {
+    class Program {
+        static void Main (string[] args) {
+        
+        //     int[] g ={0,0,0,0,0,0};
+        //    string n = Convert.ToString(g);
+            
+            for (int i = 1; i <= 9; i++)
+            
+            {
+                string b = string.Format("{0:000000}", i);
+                try{
+                    string result = string.Parse(b);
+                    Console.WriteLine(result);
+
+                }catch(FormatException)
+                {
+                    Console.WriteLine($"Unable to parse '{b}'");
+
+                }
+                
+                
+                
+                if (b[4]+b[5]=="9") 
+                {
+                    
+                     Console.WriteLine(b);
+                }else
+                {
+                    Console.WriteLine("x");
+                }
+                
+             }
+             //Console.ReadLine();           
+        }
+     }
+  }
