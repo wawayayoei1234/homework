447218de1b75c7f5944ac6693d725a90622b14d5
wawayayoei1234 <56579760+wawayayoei1234@users.noreply.github.com>
2019-10-15 14:55:32 +0700

    Add files via upload

---
+++ homework1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+
+namespace d3
+{
+    public interface IHomework05
+    {
+    string DisplayLEDOnScreen(string ledNo);
+    }   
+        
+    public class homewark : IHomework05
+    {
+
+        public string DisplayLEDOnScreen(string ledNo)
+        {   
+            Console.WriteLine("welcom"+ ledNo);
+            int x = 0;
+            string[] d = { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
+            int[] g = { 0,0,0,0,0,0,0,0,0,0};
+            string e = (" 1  2  3  4  5  6  7  8  9  A");
+                while( x < 10 )
+                {
+                     int b = 1;
+                     string input = Console.ReadLine();
+                     string sum = null;
+                     switch(input) 
+                {
+                case "1":
+                     if(g[0] == b ){
+                        d[0]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                       
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[0] = 0;
+                          
+                    }
+                    else
+                    { 
+                        d[0]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[0] = 1;
+                        
+                    }
+                    break;
+                    case "2":
+                     if(g[1] == b ){
+                        d[1]="[ ]";
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[1] = 0;
+                       
+                    }
+                    else
+                    { 
+                        d[1]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[1] = 1;
+                        
+                    }
+                    break;
+                    case "3":
+                     if(g[2] == b ){
+                        d[2]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                       
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[2] = 0;
+                      
+                    }
+                    else
+                    { 
+                        d[2]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[2] = 1;
+                       
+                    }
+                    break;
+                    case "4":
+                     if(g[3] == b ){
+                        d[3]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                      
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[3] = 0;
+                        
+                    }
+                    else
+                    { 
+                        d[3]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[3] = 1;
+                        
+                    }
+                    break;
+                    case "5":
+                     if(g[4] == b ){
+                        d[4]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                        
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[4] = 0;
+                        
+                    }
+                    else
+                    { 
+                        d[4]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[4] = 1;
+                        
+                    }
+                    break;
+                    case "6":
+                     if(g[5] == b ){
+                        d[5]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                 
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[5] = 0;
+                      
+                    }
+                    else
+                    { 
+                        d[5]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[5] = 1;
+                      
+                    }
+                    break;
+                    case "7":
+                     if(g[6] == b ){
+                        d[6]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                       
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[6] = 0;
+                     
+                    }
+                    else
+                    { 
+                        d[6]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[6] = 1;
+                        
+                    }
+                    break;
+                    case "8":
+                     if(g[7] == b ){
+                        d[7]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                      
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[7] = 0;
+                      
+                    }
+                    else
+                    { 
+                        d[7]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[7] = 1;
+                     
+                    }
+                    break;
+                    case "9":
+                     if(g[8] == b ){
+                        d[8]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                       
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[8] = 0;
+                        
+                    }
+                    else
+                    { 
+                        d[8]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[8] = 1;
+                        
+                    }
+                    break;
+                     case "A":
+                     if(g[9] == b ){
+                        d[9]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                       
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[8] = 0;
+                        
+                    }
+                    else
+                    { 
+                        d[9]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[9] = 1;     
+                    }
+                    break;
+                    case "10":
+                     if(g[9] == b ){
+                        d[9]="[ ]";
+                        foreach (string item in d)
+                        sum += item;                       
+                        System.Console.WriteLine(sum);
+                        Console.WriteLine(e);                
+                        g[8] = 0;
+                        
+                    }
+                    else
+                    { 
+                        d[9]="[!]"; 
+                        foreach (string item in d)
+                        sum += item;
+                        System.Console.WriteLine(sum);       
+                        Console.WriteLine(e);          
+                        g[9] = 1;     
+                    }
+                    break;
+                }
+                    
+                
+            }
+            return ledNo;
+        }      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            homewark yayo = new homewark();
+            yayo.DisplayLEDOnScreen("wawa");
+            
+            
+        }
+    }       
+}
