32e2f9b26bd65ebce53651b175a1602ea13fd76a
vissavut <51730920+vissavut@users.noreply.github.com>
2019-10-25 16:53:51 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -39,19 +39,15 @@ namespace test
                
                    
                   foreach (var item in oop)
-                 {
+                 
                  nums+=item;
-                 }  
+                  
                     
                  
                 }
-                var i1 = nums;
-                var i2 = $"{i1[0]}{i1[1]}{i1[2]}{i1[3]}{i1[4]}{i1[5]}";
-                var i3 = $"{i1[6]}{i1[7]}{i1[8]}{i1[9]}{i1[10]}{i1[11]}";
-                var i4 = $"{i1[12]}{i1[13]}{i1[14]}{i1[15]}{i1[16]}{i1[17]}";
-                var i5 = $"{i1[18]}{i1[19]}{i1[20]}{i1[21]}{i1[22]}{i1[23]}";
-                Console.WriteLine("ค่าที่มีเลข 99 ต่อกัน 2 ตัว");
-                Console.WriteLine($"{i2} {i3} {i4} {i5}");
+            Console.WriteLine("ค่าที่มีเลข 99 ต่อกัน 2 ตัว");
+              Console.WriteLine(nums);
+                
 
                        
                    
