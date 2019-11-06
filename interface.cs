9cd294e0d3f3774118a92db5a8411a49ec444328
wawayayoei1234 <56579760+wawayayoei1234@users.noreply.github.com>
2019-11-06 14:03:09 +0700

    Add files via upload

---
+++ interface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace interfacetrying
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        int x = 1;
+        while (x < 10)
+        {
+            IHomework05 yayo = new homewark();
+            String ledNo = Console.ReadLine();
+            string waa = yayo.DisplayLEDOnScreen(ledNo);
+            Console.WriteLine(waa);
+        }
+    }
+    }
+}
