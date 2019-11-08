78c73a9200bbfc19eb5a6559e8e8bab6919cc2c1
wawayayoei1234 <56579760+wawayayoei1234@users.noreply.github.com>
2019-11-08 10:27:53 +0700

    ตัวอย่าง

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace interfacetrying
+
+namespace rename
 {
     class Program
     {
         static void Main(string[] args)
-    {
-        int x = 1;
-        while (x < 10)
         {
-            IHomework05 yayo = new homewark();
-            String ledNo = Console.ReadLine();
-            string waa = yayo.DisplayLEDOnScreen(ledNo);
-            Console.WriteLine(waa);
+            IEnumerable<int> wawa = new List<int>();
+            IHomework13 re = new homework2();
+            var number = new List<int>(){000000, 000099, 009900, 990000,
+            000999, 009990, 099900, 999000,
+            990999, 919909, 999799, 999999,};
+            wawa = re.Homework13(number);
+            foreach (var item in wawa)
+            {
+                string r = String.Format("{0:D6}", item);
+                Console.WriteLine(r);
+            }
+
         }
-    }
     }
 }
