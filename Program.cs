105970b13431ed34f39cccfd5eecec2c8edf9f2a
wawayayoei1234 <56579760+wawayayoei1234@users.noreply.github.com>
2019-11-08 10:26:30 +0700

    ตัวอย่าง

--- Program.cs
+++ Program.cs
@@ -1,90 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace test
+namespace interfacetrying
 {
     class Program
     {
         static void Main(string[] args)
-
+    {
+        int x = 1;
+        while (x < 10)
         {
-
-
-
-            IEnumerable<int> oop = null;
-            string nums = null;// string.Empty;
-            string myString = string.Empty;
-            
-            
-            int myInt;
-
-
-         
-             
-            int max = 4;
-           
-                Console.WriteLine("ป้อนค่่าตัวเลข 6 หลัก จำนวน 4 ค่า");
-            for (int i = 0; i < max; i++)
-            {
-                  myString = Console.ReadLine();  //รับค่า
-
-                  var array = myString.ToCharArray() .Where(x => int.TryParse
-                            (x.ToString(), out myInt))
-                            .Select(x => int.Parse
-                            (x.ToString())).ToArray();  //แปลง string to array
-
-                   var input = new run();        
-                   oop = input.Homework13(array);
-               
-                   
-                  foreach (var item in oop)
-                 
-                 nums+=item;
-                  
-                    
-                 
-                }
-            Console.WriteLine("ค่าที่มีเลข 99 ต่อกัน 2 ตัว");
-              Console.WriteLine(nums);
-                
-
-                       
-                   
-                  
-                     
+            IHomework05 yayo = new homewark();
+            String ledNo = Console.ReadLine();
+            string waa = yayo.DisplayLEDOnScreen(ledNo);
+            Console.WriteLine(waa);
         }
     }
+    }
 }
-            
-         
-          
-                
-                     
-                 
-
-            
-                 
-                      
-            
-            
-                   
-
-                  
-                       
-                     
-               
-                       
-            
-        
-                 
-                   
-           
-
-
-
-
-
-
-
-
